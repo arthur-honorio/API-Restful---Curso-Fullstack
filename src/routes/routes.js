04b3ec866c11200de3238ab86b32599219690645
Arthur Honorio <thudo.novo@gmail.com>
2021-08-14 00:16:33 -0300

    adicionando metodo get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const ProductsController = require("../controllers/products")
 
-router.get("/products", ProductsController.get)
+router.get("/products/:id?", ProductsController.get)
 // router.post("/clientes", ProductsController.post)
 // router.put("/clientes/:id", ProductsController.put)
 // router.delete("/clientes/:id", ProductsController.delete)
