5707522313375d83eb06e48e00fc5a7bc9c31e55
Arthur Honorio <thudo.novo@gmail.com>
2021-08-14 20:20:37 -0300

    adicionando metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -24,12 +24,47 @@ async function post(req, res) {
 
   product.save()
   res.send({
-    message: "success"
+    message: "Success"
   })
 
 }
 
+async function put(req, res) {
+  const { id } = req.params
+
+  // Mostra, no BackEnd, o produto já atualizado
+  const product = await ProductsModel.findOneAndUpdate({ _id: id }, req.body, { new: true })
+
+/*
+  Mostra, no BakcEnd, o produto antes de atualizar
+
+  const product = await ProductsModel.findOne({ _id: id })
+  await product.updateOne(req.body)
+*/
+  res.send({
+    message: "Success",
+    product,
+  }) 
+
+}
+
+async function remove(req, res) {
+  const { id } = req.params
+
+  const remove = await ProductsModel.deleteOne({ _id: id})
+
+  const message = remove.ok ? "Success" : "Error"
+
+  res.send ({
+    message,
+  })
+}
+
+
+
 module.exports = { 
   get,
   post,
+  put,
+  remove
 }
